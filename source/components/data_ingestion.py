3edad460d72ffc600b9e41f97d93ed704494277e
domohal <dom.ohalloran86@gmail.com>
2024-06-10 15:45:07 +0100

    ingestion

--- source/components/data_ingestion.py
+++ source/components/data_ingestion.py
@@ -1,17 +1,17 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+from source.exception import CustomException
+from source.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+from source.components.data_transformation import DataTransformation
+from source.components.data_transformation import DataTransformationConfig
 
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
+from source.components.model_trainer import ModelTrainerConfig
+from source.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
